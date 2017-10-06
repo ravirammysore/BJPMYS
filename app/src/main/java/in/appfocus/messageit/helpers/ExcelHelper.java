830381fb4693249c405f09314c2048ac6a02ac01
Ravi K R <raviram.mysore@gmail.com>
2017-10-06 17:11:30 +0530

    tested saving an excel file which wil be useful for export feature to be done later, works fine

--- app/src/main/java/in/appfocus/messageit/helpers/ExcelHelper.java
+++ app/src/main/java/in/appfocus/messageit/helpers/ExcelHelper.java
@@ -9,19 +9,27 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Toast;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.poifs.filesystem.OfficeXmlFileException;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.util.Iterator;
 
@@ -31,10 +39,10 @@ import io.realm.Realm;
 import io.realm.RealmList;
 
 public class ExcelHelper {
-    //was present in downloaded code
-    //private static File EXCEL_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-    //private static String filename = "importtest.xls";
-
+    
+    //Create a path where we will save our excel files
+    private static File EXCEL_EXPORT_PATH = Environment.getExternalStorageDirectory();
+    
     private static RealmList<Contact> lstContactsToImportFromExcel;
 
     public static RealmList<Contact> extractContactsInExcel(Context context, Realm realm, String groupId, String excelFilePath) {
@@ -170,6 +178,77 @@ public class ExcelHelper {
             e.printStackTrace();
         }
         return result;
+    }
+
+    //// TODO: 06-10-2017 export feature 
+    public static boolean saveExcelFile(Context context, String fileName) {
+
+        // check if available and not read only
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            Log.w("FileUtils", "Storage not available or read only");
+            Toast.makeText(context, "Storage not available or read only", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        boolean success = false;
+
+        //New Workbook
+        Workbook wb = new HSSFWorkbook();
+
+        Cell c = null;
+
+        //Cell style for header row
+        //CellStyle cs = wb.createCellStyle();
+        //cs.setFillForegroundColor(HSSFColor.LIME.index);
+        //cs.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+
+        //New Sheet
+        Sheet sheet1 = null;
+        sheet1 = wb.createSheet("myOrder");
+
+        // Generate column headings
+        Row row = sheet1.createRow(0);
+
+        c = row.createCell(0);
+        c.setCellValue("Item Number");
+        //c.setCellStyle(cs);
+
+        c = row.createCell(1);
+        c.setCellValue("Quantity");
+        //c.setCellStyle(cs);
+
+        c = row.createCell(2);
+        c.setCellValue("Price");
+        //c.setCellStyle(cs);
+
+        sheet1.setColumnWidth(0, (15 * 500));
+        sheet1.setColumnWidth(1, (15 * 500));
+        sheet1.setColumnWidth(2, (15 * 500));
+        
+        //File file = new File(context.getExternalFilesDir(null), fileName);
+        File file = new File(EXCEL_EXPORT_PATH, fileName);
+        FileOutputStream os = null;
+
+        try {
+            os = new FileOutputStream(file);
+            wb.write(os);
+            Log.w("FileUtils", "Writing file" + file);
+            success = true;
+        } catch (IOException e) {
+            Log.w("FileUtils", "Error writing " + file, e);
+        } catch (Exception e) {
+            Log.w("FileUtils", "Failed to save file", e);
+        } finally {
+            try {
+                if (null != os)
+                    os.close();
+                Toast.makeText(context, "Done!", Toast.LENGTH_SHORT).show();
+            } 
+            catch (Exception ex) {
+            }
+        }
+
+        return success;
     }
 
 }
